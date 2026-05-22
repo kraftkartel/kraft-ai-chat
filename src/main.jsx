0b0623f322f71658793e830b02cb7e1589a5510f
kraftkartel <kraftkartel20@gmail.com>
2026-05-22 09:06:50 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import KraftLogin from "./KraftLogin.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function Root() {
+  const [unlocked, setUnlocked] = useState(
+    sessionStorage.getItem("kraft_unlocked") === "1"
+  );
+  return unlocked ? <App /> : (
+    <KraftLogin onUnlock={() => {
+      sessionStorage.setItem("kraft_unlocked", "1");
+      setUnlocked(true);
+    }} />
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
-)
+);
